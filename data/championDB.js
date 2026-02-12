9d503d8d49c976774c1b96e0d69d97f1f67e29d8
MazaxTH <salvesthiago2000@gmail.com>
2026-02-12 20:15:30 -0300

    ralia-DEF-nerfed-from-75-to-70

--- data/championDB.js
+++ data/championDB.js
@@ -9,7 +9,7 @@ export const championDB = {
     portrait: "assets/portraits/ralia.png",
     HP: 365,
     Attack: 45,
-    Defense: 75,
+    Defense: 70,
     Speed: 40,
     Critical: 0,
     LifeSteal: 15,
