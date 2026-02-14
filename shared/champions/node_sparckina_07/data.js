f82ada9ce4469cb5a50ae27ae4de29761bf7f3bb
salvesMazaxTH <salvesthiago2000@gmail.com>
2026-02-14 13:44:59 -0300

    sparckina-huge-ATK-buff

--- shared/champions/node_sparckina_07/data.js
+++ shared/champions/node_sparckina_07/data.js
@@ -2,7 +2,7 @@ export default {
   name: "Node-SPARCKINA-07",
   portrait: "/assets/portraits/node_sparckina_07.png",
   HP: 310,
-  Attack: 50,
+  Attack: 80,
   Defense: 50,
   Speed: 75,
   Critical: 0,
