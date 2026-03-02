a90e6a87344cb4bbda3dc8bf8b19c6e8f4f3a275
Thiago <mazaxbr@gmail.com>
2026-03-02 19:38:47 -0300

    naelys-enabled-but-not-working

--- shared/champions/elias_cross/data.js
+++ shared/champions/elias_cross/data.js
@@ -1,12 +1,12 @@
 export default {
   name: "Elias Cross",
   portrait: "/assets/portraits/elias_cross.webp",
+  unreleased: true,
 
   HP: 300,
   Attack: 295,
   Defense: 70,
   Speed: 90,
-  Evasion: 1,
   
   elementalAffinities: ["lightning"],
 };
