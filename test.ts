4fd8cab600fcc558c3eea48c55b8d989dd11a1f6
Thiago <mazaxbr@gmail.com>
2026-03-27 12:42:13 -0300

    v3.3

--- test.ts
+++ test.ts
@@ -1,1 +1,6 @@
-const a: number = 1;
+const v = { currentVersion: "v3.3" };
+
+console.log(v.currentVersion.split(".")[1]);
+console.log(v.currentVersion.split(".")[2]);
+console.log(v.currentVersion.split(".")[3]);
+
